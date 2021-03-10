75357c8f8ecd383a6668e8a6fdc2dfe1ae29b1a3
rzgeer <67606150+rzgeer@users.noreply.github.com>
2021-03-09 22:19:52 -0500

    Completes Project

---
+++ UI/Instructions.gd
@@ -0,0 +1,20 @@
+extends Control
+
+var countdown = 5
+
+func _ready():
+	update_label()
+
+
+
+func _on_Countdown_timeout():
+	countdown -= 1 
+	update_label()
+
+func update_label():
+	$Countdown_Label.text = "Starting in " + str(countdown)
+	if countdown <= 0:
+		hide()
+		$Countdown.stop()
+		get_tree().paused = false
+		
